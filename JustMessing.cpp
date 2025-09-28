e29f492ad1f9e3b08421e289adccf12555218f08
Cavan Theiss <cavanrt@gmail.com>
2025-09-27 19:21:22 -0500

    Create JustMessing.cpp

---
+++ JustMessing.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+/*
+
+The first line of the input contains the number of integers . The next line contains  integers in sorted order. The next line contains , the number of queries. Then  lines follow each containing a single integer .
+
+Note: If the same number is present multiple times, you have to print the first index at which it occurs. Also, the input is such that you always have an answer for each query. **CLEAN THIS UP TOMORROW**
+
+*/
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
+}
