29957820d6888bad6ed755960a20d44724e19538
Cavan Theiss <cavanrt@gmail.com>
2025-09-28 08:00:08 -0500

    Update TemplatePractice.cpp

--- TemplatePractice.cpp
+++ TemplatePractice.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 
+template <typename T>
+T add(T a, T b) {
+    return a + b;
+}
+
 using namespace std;
 
 int main() {
-
-  return 0;
+    
+    cout << add(5, 7) << endl;
+    
+    cout << add(5.3, 7.7) << endl;
+    
 }
 
 
+ 
