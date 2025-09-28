ac6eaf248d64836542fc06549a07e66b16cded82
Cavan Theiss <cavanrt@gmail.com>
2025-09-28 08:17:06 -0500

    Update TemplatePractice.cpp

--- TemplatePractice.cpp
+++ TemplatePractice.cpp
@@ -8,11 +8,21 @@ class Calculator {
  
  T add(T a, T b) {
     return a + b;
-} 
+}
+
  T subtract(T a, T b) {
     return a - b;
-} 
-    
+}
+
+ T multiply(T a, T b) {
+    return a * b;
+}
+
+ T divide(T a, T b) {
+    return a / b;
+}
+ 
+ 
 };
 
 using namespace std;
@@ -23,13 +33,14 @@ int main() {
     
     cout << intCalculator.add(5, 8) << endl;
     cout << intCalculator.subtract(5, 8) << endl;
+    cout << intCalculator.multiply(5, 8) << endl;
+    cout << intCalculator.divide(5, 8) << endl << endl;
     
     Calculator<float> floatCalculator;
     
     cout << floatCalculator.add(5.2, 7.84) << endl;
     cout << floatCalculator.subtract(5.2, 7.84) << endl;
+    cout << floatCalculator.multiply(5.2, 7.84) << endl;
+    cout << floatCalculator.divide(5.2, 7.84) << endl;
     
 }
-
-
- 
