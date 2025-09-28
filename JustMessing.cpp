ee3f0cf57809aba2287147323dd9bc53e7b35f7e
Cavan Theiss <cavanrt@gmail.com>
2025-09-27 19:47:44 -0500

    Update JustMessing.cpp

--- JustMessing.cpp
+++ JustMessing.cpp
@@ -1,6 +1,11 @@
 #include <vector>
+#include <iomanip>
 #include <iostream>
 using namespace std;
+
+
+// this is just a dumb program that breaks when you pick a number larger than 4 or 5
+// and technically it doesnt break, it just overwelms the screen
 
 
 int main() {
@@ -10,12 +15,12 @@ int main() {
     cout << "Please pick a number" << endl << "    :";
     cin >> number1;
 
-    cout << endl << "ok, lets have some fun" << endl;
-    for(int i = 1; i <= number1; i++){
-        for(int j = 1; j <= number1; j++){
-            cout << "    " << i*j << "    ";
+    cout << endl << "ok, lets have some fun" << endl << endl;
+    for(double i = 1; i <= number1; i = i + .25){
+        for(double j = 1; j <= number1; j = j + .25){
+            cout << left << setw(15) << i*j << setw(15);
         }
-        cout << endl;
+        cout << endl << endl;
     }
     
     return 0;
