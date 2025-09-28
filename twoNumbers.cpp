65206244fea1950c5e9879f5837028788457e9fe
Cavan Theiss <cavanrt@gmail.com>
2025-09-28 07:32:23 -0500

    Update twoNumbers.cpp

--- twoNumbers.cpp
+++ twoNumbers.cpp
@@ -4,7 +4,12 @@ using namespace std;
 
 int main () {
 
+int number1, number2;
 
+  cout << "pick a number" << endl << "  :";
+  cin >> number1;
+  cout << endl << endl << "Pick another number" << endl << "  :";
+  cin >> number2
 
 
   return 0;
