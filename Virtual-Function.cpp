e10e51bd671c2fc23c52c189a977b3c0b89d010d
Cavan Theiss <cavanrt@gmail.com>
2025-09-29 12:18:04 -0500

    Update Virtual-Function.cpp

--- Virtual-Function.cpp
+++ Virtual-Function.cpp
@@ -1,10 +1,20 @@
 #include<iostream>
 using namespace std;
 
+class Instrument {
+
+public:
+  void MakeSound(){
+    cout << "Instrument playing...\n";
+    
+  }
+
+};
 
 int main() 
 {
-
+  Instrument i1;
+  i1.MakeSound();
 
   system ("pause>0");
 }
