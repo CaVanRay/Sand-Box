b699cce6fde8f8fa924cb49c7b96ee492b19fff6
Cavan Theiss <cavanrt@gmail.com>
2025-09-29 12:44:04 -0500

    Update Virtual-Function.cpp

--- Virtual-Function.cpp
+++ Virtual-Function.cpp
@@ -4,9 +4,7 @@ using namespace std;
 class Instrument {
 
 public:
-  virtual void MakeSound(){
-    cout << "Instrument playing...\n";    
-  }
+  virtual void MakeSound() = 0;
 };
 
 class Accordion:public Instrument {
@@ -15,12 +13,19 @@ class Accordion:public Instrument {
   }
 };
 
+class Piano :public Instrument{
+void MakeSound(){
+  cout << "Piano playing...\n";
+}
+};
+
 int main() 
 {
   Instrument* i1 = new Accordion();
   i1->MakeSound();
 
-  
+    Instrument* i2 = new Piano();
+  i2->MakeSound();
 
   return 0;
 }
