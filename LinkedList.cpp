237653cf3276b95573b7d2f895e96a0ef958aa08
Cavan Theiss <cavanrt@gmail.com>
2025-09-30 20:15:57 -0500

    Update LinkedList.cpp

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -10,7 +10,24 @@ struct Node {
 
 int main() {
 
+int main() {
+    // Create nodes
+    Node* head = new Node();   // first node
+    Node* second = new Node(); // second node
+    Node* third = new Node();  // third node
 
+    // Assign values
+    head->data = 10;
+    head->next = second;
+
+    second->data = 20;
+    second->next = third;
+
+    third->data = 30;
+    third->next = nullptr; // end of list
+
+    return 0;
+}
 
 
 
