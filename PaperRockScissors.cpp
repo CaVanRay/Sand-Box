76ca284af836f46ef8816af49e088457e980bfba
Cavan Theiss <cavanrt@gmail.com>
2025-10-05 19:47:51 -0500

    Update PaperRockScissors.cpp

--- PaperRockScissors.cpp
+++ PaperRockScissors.cpp
@@ -6,13 +6,24 @@ using namespace std;
 
 int main() {
 
-
+  srand(time(NULL));
   cout << "Want to play a game of paper, rock, scissors? " << \n;
   char answ;
   cin >> answ;
   cout << "Pick a weapon and then click enter" << \n;
   cin.get();
-  
+  int weapon = ((rand() % 3) + 1)
+
+  switch(weapon){
+   case 1:
+            std::cout << "Paper" << std::endl;
+            break;
+        case 2:
+            std::cout << "Rock" << std::endl;
+            break;
+        case 3:
+            std::cout << "Scissors" << std::endl;
+            break;
   
 
   return 0;
