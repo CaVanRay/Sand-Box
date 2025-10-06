b413c114a9e5bdbe3eeeef896121affad32303de
Cavan Theiss <cavanrt@gmail.com>
2025-10-05 19:39:36 -0500

    Update PaperRockScissors.cpp

--- PaperRockScissors.cpp
+++ PaperRockScissors.cpp
@@ -4,5 +4,13 @@ using namespace std;
 
 int main() {
 
+
+  cout << "Want to play a game of paper, rock, scissors? " << \n;
+  char answ;
+  cin >> answ;
+  cout << "Pick a weapon and then click enter" << \n;
+  cin >> ;
+  
+
   return 0;
 }
