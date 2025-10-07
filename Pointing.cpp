8dace00b5beed8bbf68e712d1020b612c97e21a5
Cavan Theiss <cavanrt@gmail.com>
2025-10-07 11:58:25 -0500

    Update Pointing.cpp

--- Pointing.cpp
+++ Pointing.cpp
@@ -4,6 +4,17 @@ using namespace std;
 
 int main() {
 
+  int num1;
+  double num2;
+  char letter;
+  string letters;
+
+  int* Pnum1 = &num1;
+  double* Pnum2 = &num2;
+  char* Pletter = &letter;
+  string* Pletters = &letters;
+
+  
 
   return 0;
 }
