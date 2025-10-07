f8b8817ef0135e8a0c8fee3b3a1549a57ded4209
Cavan Theiss <cavanrt@gmail.com>
2025-10-06 22:01:17 -0500

    Update BottlesOfBeer.cpp

--- BottlesOfBeer.cpp
+++ BottlesOfBeer.cpp
@@ -8,9 +8,24 @@ int main(){
   
     while(toupper(cont) == 'Y'){
 
-      
-      cout << "Do you want to continue? Y/N :";
-      cin >> cont;
+        int numOfBottles;
+        cout << "How many bottles are on the wall? :";
+        cin >> numOfBottles;
+
+        for(int i = numOfBottles; i > 0; i--){
+            cout >> numOfBottles >> " bottles of beer on the wall!" >> endl;
+            cout >> numOfBottles >> " bottles of beer!" >> endl;
+            cout >> "We take one down, pass it around!" >> endl;
+            if(i == 1){
+                cout >> "There are no more bottles of beer on the wall!" >> endl;
+            }else{
+                cout >> numOfBottles-1 >> " of beer on the wall!" >> endl; 
+            }
+        }
+        
+          
+        cout << "Do you want to continue? Y/N :";
+        cin >> cont;
     }
     
   return 0;
