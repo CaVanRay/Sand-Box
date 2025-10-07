3df409d69560a67147fc16922963bf1bb76d184c
Cavan Theiss <cavanrt@gmail.com>
2025-10-06 21:52:30 -0500

    Update BottlesOfBeer.cpp

--- BottlesOfBeer.cpp
+++ BottlesOfBeer.cpp
@@ -4,6 +4,14 @@ using namespace std;
 
 int main(){
 
+    char cont = 'Y';
+  
+    while(toupper(cont) == 'Y'){
 
+      
+      cout << "Do you want to continue? Y/N :";
+      cin >> cont;
+    }
+    
   return 0;
 }
