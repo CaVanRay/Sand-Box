75ff0af49cbb2466c5a74bd55fd5f90aff5f55db
Cavan Theiss <cavanrt@gmail.com>
2025-10-07 11:50:16 -0500

    Update Pointers.cpp

--- Pointers.cpp
+++ Pointers.cpp
@@ -4,6 +4,15 @@ using namespace std;
 
 int main(){
 
+  // I need to practice with pointers
 
+  int PotOGold;
+  int* EndOfRainbow;
+  EndOfRainbow = &PotOGold;
+
+  PotOGold = 999;
+  
+  cout << "At the end of the rainbow theres a pot of " << EndOfRainbow << " peices of gold!" << endl;
+  
   return 0;
 }
