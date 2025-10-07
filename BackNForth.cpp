bf96ea2f26a3c154b44bd82a3fac62359d9b1e08
Cavan Theiss <cavanrt@gmail.com>
2025-10-06 21:25:00 -0500

    Update BackNForth.cpp

--- BackNForth.cpp
+++ BackNForth.cpp
@@ -4,6 +4,15 @@ using namespace std;
 
 int main() {
 
+  char cont = Y;
+
+  while(toupper(cont) == Y) {
+
+
+
+    cout << "Would you like to continue? Y/N" << endl << endl;
+    cin >> cont;
+  }
 
   return 0;
 }
