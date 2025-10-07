00bba6e65c94b5b3ee86f81b262fc0b0a11f40c3
Cavan Theiss <cavanrt@gmail.com>
2025-10-06 21:26:28 -0500

    Update BackNForth.cpp

--- BackNForth.cpp
+++ BackNForth.cpp
@@ -8,6 +8,9 @@ int main() {
 
   while(toupper(cont) == Y) {
 
+    int items;
+    cout << "How many items would you like to store? : " ;
+    cin >> items;
 
 
     cout << "Would you like to continue? Y/N" << endl << endl;
