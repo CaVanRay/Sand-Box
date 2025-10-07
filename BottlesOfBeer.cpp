d2e8d41ab011d542daadd64c8ab4877557f95e93
Cavan Theiss <cavanrt@gmail.com>
2025-10-06 22:03:50 -0500

    Update BottlesOfBeer.cpp

--- BottlesOfBeer.cpp
+++ BottlesOfBeer.cpp
@@ -13,13 +13,13 @@ int main(){
         cin >> numOfBottles;
 
         for(int i = numOfBottles; i > 0; i--){
-            cout >> numOfBottles >> " bottles of beer on the wall!" >> endl;
-            cout >> numOfBottles >> " bottles of beer!" >> endl;
-            cout >> "We take one down, pass it around!" >> endl;
+            cout << numOfBottles << " bottles of beer on the wall!" << endl;
+            cout << numOfBottles << " bottles of beer!" << endl;
+            cout << "We take one down, pass it around!" << endl;
             if(i == 1){
-                cout >> "There are no more bottles of beer on the wall!" >> endl;
+                cout << "There are no more bottles of beer on the wall!" << endl;
             }else{
-                cout >> numOfBottles-1 >> " of beer on the wall!" >> endl; 
+                cout << numOfBottles-1 << " of beer on the wall!" << endl; 
             }
         }
         
