e0e1ca2a70fe8542ad3df1789f168c937f878aa3
Cavan Theiss <cavanrt@gmail.com>
2025-10-08 20:08:05 -0500

    Update ShuffleCups.cpp

--- ShuffleCups.cpp
+++ ShuffleCups.cpp
@@ -12,15 +12,19 @@ srand(time(0));
 char answer = 'Y';
 while(toupper(answer) == 'Y'){
 
-    cout << endl*3;
+    cout << endl << endl << endl;
+    cout << "Watch me put the ball under cup 2";
 
     int cup[3] = {1, 2, 3};  
     int* ball = &cup[2];
     
-  
+    cout << endl << endl << endl;
+    cout << "And now I shuffle the cups";
     
     int randomNumber = (rand() % 3);
     ball = &cup[randomNumber];
+    
+    
     cout << endl << endl << "The Ball is located in cup " << *ball ;
     cout << endl << endl << "Would you like to continue? (Y/N): " ;
     cin >> answer;
