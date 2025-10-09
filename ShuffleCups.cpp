37080090e27ad4ab3b3b283333e3b5d6fde72c33
Cavan Theiss <cavanrt@gmail.com>
2025-10-08 20:03:41 -0500

    Update ShuffleCups.cpp

--- ShuffleCups.cpp
+++ ShuffleCups.cpp
@@ -8,21 +8,23 @@ using namespace std;
 
 int main(){
     
-  srand(time(0));
-  int cup[3] = {1, 2, 3};
+srand(time(0));
+
+while(toupper(answer) == 'Y'){
+
+    cout << endl*3;
+
+    int cup[3] = {1, 2, 3};  
+    int* ball = &cup[2];
+    char answer = 'Y';
   
-  int* ball = &cup[2];
-  char answer = 'Y';
-  
-  while(toupper(answer) == 'Y'){
     
     int randomNumber = (rand() % 3);
     ball = &cup[randomNumber];
     cout << endl << endl << "The Ball is located in cup " << *ball ;
-    
     cout << endl << endl << "Would you like to continue? (Y/N): " ;
     cin >> answer;
-  }
+}
   
   
   return 0;
