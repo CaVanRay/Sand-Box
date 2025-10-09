f5bb405ab4c1ce18d49b0d5889e361b0eddd87f3
Cavan Theiss <cavanrt@gmail.com>
2025-10-08 19:33:34 -0500

    Update ShuffleCups.cpp

--- ShuffleCups.cpp
+++ ShuffleCups.cpp
@@ -3,7 +3,17 @@
 using namespace std
 
 int main(){
+
+  int cup1 = 1, cup2 = 2, cup3 = 3;
+  int* ball = &cup2;
+  char answer = 'Y';
   
+  while(toupper(answer) == 'Y'){
+
+
+    cout << endl << endl << "Would you like to continue? (Y/N): " ;
+    cin >> answer;
+  }
   
   
   return 0;
