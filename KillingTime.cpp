ada290c27635cb00a92ffacd6bbcb57ee1d700c5
Cavan Theiss <cavanrt@gmail.com>
2025-10-14 15:35:00 -0500

    Update KillingTime.cpp

--- KillingTime.cpp
+++ KillingTime.cpp
@@ -3,14 +3,27 @@
 class car{
 private:
 
+string ownerName;
+int mileage;
+int MPG;
 
 public:
 
+GetOwner();
+GetMileage();
+GetMPG();
+SetOwner();
+SetMileage();
+SetMPG();
 
-}
+};
 
 int main() {
-
+ 
+ car Mustang;
+ car Impala;
+ car Sunfire;
+ 
 
  return 0;
 }
