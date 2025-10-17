ec1adb69721413b621a206a356d16f5de1f52699
Cavan Theiss <cavanrt@gmail.com>
2025-10-16 19:06:48 -0500

    Update TBD.cpp

--- TBD.cpp
+++ TBD.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
 
 class car{
-
+  private:
+  string Owner;
+  string Model;
+  string Vin;
+  int Mileage;
+  int MPG;
+  public:
+  
+  
 };
 
 int main(){
-
+  char cont = "Y";
   do{
-    
+
+
+    std::cout << std::endl << std::endl << "Continue? (Y/N) :" 
+    std::cin >> cont;
   }while(toupper(cont)=='Y';
   
   return 0;
