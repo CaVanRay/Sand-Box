c393d148f741e96b599e4cfbd6f9827e71f90458
Cavan Theiss <cavanrt@gmail.com>
2025-10-17 16:28:36 -0500

    Update TBD.cpp

--- TBD.cpp
+++ TBD.cpp
@@ -57,7 +57,20 @@ class car{
     };
   };
 //***********************************************************************
-
+void printCataloge(car* n) {
+    while (n != nullptr) {
+        std::cout << std::endl << "***************************************" << std::endl;
+        std::cout << "Owner: " << n->GetOwner() << std::endl;
+        std::cout << "Model: " << n->GetModel() << std::endl;
+        std::cout << "VIN# : " << n->GetVin() << std::endl;
+        std::cout << "Miles: " << n->GetMileage() << std::endl;
+        std::cout << "MPG  : " << n->GetMPG
+        cout << n->getData() << " ";
+        n = n->GetNext();
+    }
+    cout << endl;
+}
+//***********************************************************************
 int main(){
   char conti;
   do{
